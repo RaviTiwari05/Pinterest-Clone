43ccfadcd90880568250059bb95a11fe54fa4af2
RaviTIwari05 <ravifzd05@gmail.com>
2024-05-26 14:10:38 +0530

    Register page was added

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
-const mongoose = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/pin");
-
-const userSchema = mongoose.Schema({
-  
+var express = require('express');
+var router = express.Router();
+router.get('/', function(req,res,next){
+    res.send('respond with a resource');
 });
+
+module.exports = router;
